dd7808b39802fdaaac8787f4ae60ff3fff9a583f
严俊东 <1172482914@qq.com>
2019-01-05 19:13:59 +0800

    教练姓名唯一

--- app/carTrainer/carTrainerService.js
+++ app/carTrainer/carTrainerService.js
@@ -1,7 +1,32 @@
 const CarTrainer = require('./carTrainerSchema')
 const baseService = require('../base/baseService')
+const code = require('../code')
 
 module.exports = {
     DB: CarTrainer,
-    ...baseService
+    ...baseService,
+    /** 增加 */
+    async add(ctx, object) {
+        let trainer = await this.DB.findOne({
+            name: object.name
+        })
+        ctx.assert(!trainer, code.BadRequest, '该姓名已存在，请尝试姓名后面添加数字。<br/>如：张三1')
+        object = new this.DB(object)
+        object.admin = ctx.state.admin
+        return await object.save()
+    },
+    /** 修改 */
+    async modify(ctx, object) {
+        let trainer = await this.DB.findOne({
+            _id: {
+                $ne: object._id
+            },
+            name: object.name
+        })
+        ctx.assert(!trainer, code.BadRequest, '该姓名已存在，请尝试姓名后面添加数字。<br/>如：张三1')
+        delete object.admin
+        return await this.DB.updateOne({
+            _id: object._id
+        }, object)
+    },
 }
